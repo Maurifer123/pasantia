7a48a3492717f9918b407b627e7f8ba5174f7b72
Maurifer123 <mauriciorivamontan20@gmail.com>
2021-09-14 10:56:57 +0200

    agegando tabla, seeder y modelo personal

---
+++ database/migrations/2021_09_14_075449_create_personal_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal', function (Blueprint $table) {
+            $table->id('cip');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('celular');
+            $table->char('sexo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal');
+    }
+}
